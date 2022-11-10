e8c1b0767df76c90eced13bcd1dc6e73091dbb89
Jacob Deinum <deinum@ualberta.ca>
2022-11-10 11:22:09 -0700

    mergin

---
+++ main.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+
+def 
+
+
+
+
+def main():
+    teams = ["alberta", "bc", "twu", "tru", "manitoba", "mountroyal", 
+             "winnipeg", "brandon", "fraservalley", "saskatchewan", 
+             "macewan", "calgary", "ubco"]
+
+
+
+
