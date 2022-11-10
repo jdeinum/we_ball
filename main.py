8505c38c345a09ecc2851ea51dbe136c521b0b53
Jacob Deinum <deinum@ualberta.ca>
2022-11-10 11:25:36 -0700

    auto-merged

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 import sqlite3
 
 
-def 
+def getPage(url):
+    pass
 
 
+# if team is None, we want overall team stats from a particular year
+# otherwise we want individual stats from a particular team for a particular
+# year
+def craftUrl(team, year):
+    pass
+
+
+def extractTeamStats(response):
+    pass
+
+
+def extractIndividualStats(response):
+    pass
 
 
 def main():
-    teams = ["alberta", "bc", "twu", "tru", "manitoba", "mountroyal", 
-             "winnipeg", "brandon", "fraservalley", "saskatchewan", 
-             "macewan", "calgary", "ubco"]
+    teams = [
+        "alberta",
+        "bc",
+        "twu",
+        "tru",
+        "manitoba",
+        "mountroyal",
+        "winnipeg",
+        "brandon",
+        "fraservalley",
+        "saskatchewan",
+        "macewan",
+        "calgary",
+        "ubco",
+    ]
 
 
-
-
+if __name__ == "__main__":
+    main()
