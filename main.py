b7f9c96640470ceb307b2cf92133354cc93de8ba
Sophia Ruduke <sruduke@ualberta.ca>
2022-11-10 11:59:28 -0700

    base get and form url

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import sqlite3
+import requests
+import json
 
 
 def getPage(url):
-    pass
+    r = requests.get(url, headers={'User-Agent': 'Custom'})
+    
+    if r.status_code != 200:
+        # will deal with this better later
+        raise Exception("Request unsuccessful.")
+        return
+
+    return r
 
 
 # if team is None, we want overall team stats from a particular year
 # otherwise we want individual stats from a particular team for a particular
 # year
 def craftUrl(team, year):
-    pass
+    if team:
+        url = "https://canadawest.org/teamstats.aspx?path=mvball&year={}&school={}".format(year, team)
+    else:
+        url = "https://canadawest.org/stats.aspx?path=mvball&year={}".format(year)
+    
+    return url
 
 
 def extractTeamStats(response):
-    pass
+    stats = json.loads(response.json())
+    print(stats)
 
 
 def extractIndividualStats(response):
-    pass
+    stats = response.json()
+    print(stats)
 
 
 def main():
@@ -36,6 +52,11 @@ def main():
         "calgary",
         "ubco",
     ]
+
+    url = craftUrl(teams[0], 2022)
+    page = getPage(url)
+    extractIndividualStats(page)
+
 
 
 if __name__ == "__main__":
